83c9fb9676ecce90b352d70fc9a8a6356823d316
Grayson <Grayson@MacBook-Pro.local>
2016-12-29 08:31:49 -0500

    Made small changes to UI and added animated stripes

--- ColorBlindHelper/ViewController.swift
+++ ColorBlindHelper/ViewController.swift
@@ -3,41 +3,52 @@ import CoreImage
 import GPUImage
 import AVFoundation
 
-let testImage = UIImage(named:"stripesbw.png")!
-let pictureInput = PictureInput(image:testImage)
+var testImage = UIImage(named:"stripes1-min.png")!
+
+var pictureInput = PictureInput(image:testImage)
+
 
 class ViewController: UIViewController {
     var renderView: RenderView!
     
     let saturationFilter = SaturationAdjustment()
     let blendFilter = ChromaKeyBlend()
-    let alphaFilter = MultiplyBlend()
+    let multiplyFilter = MultiplyBlend()
     let hueFilter = HueAdjustment()
     let satFilter = SaturationAdjustment()
     let testFilter = LuminanceThreshold()
     var camera:Camera!
-    @IBOutlet weak var stripeSwitch: UISwitch!
-    @IBOutlet weak var satSlider: UISlider!
     
+    @IBOutlet weak var stripeControl: UISegmentedControl!
+    @IBOutlet weak var satSlider: UISlider!
+
+    var counter = 1
+    var timer = Timer()
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("test")
+        timer = Timer.scheduledTimer(timeInterval: 0.05, target: self, selector: #selector(ViewController.doSomeAnimation), userInfo: nil, repeats: true)
         do {
+            
             camera = try Camera(sessionPreset: AVCaptureSessionPresetHigh)
             camera.runBenchmark = true
             blendFilter.thresholdSensitivity = 0.4
             blendFilter.smoothing = 0.1
-            blendFilter.colorToReplace = Color.init(red: 1, green: 0, blue: 0, alpha: 1.0)
+            blendFilter.colorToReplace = Color.init(red: 0, green: 1, blue: 0, alpha: 1.0)
             
             satFilter.saturation = 1
+
             
-            camera --> blendFilter --> satFilter --> renderView
-            camera --> alphaFilter
-            pictureInput --> alphaFilter --> blendFilter
+            /*camera --> blendFilter --> satFilter --> renderView
+            camera --> multiplyFilter
+            pictureInput --> multiplyFilter --> blendFilter
 
 
-            pictureInput.processImage()
+            pictureInput.processImage()*/
+
             camera.startCapture()
         } catch {
             fatalError("Could not initialize rendering pipeline: \(error)")
@@ -45,15 +56,52 @@ class ViewController: UIViewController {
         
     }
     
+    func doSomeAnimation() {
+        //I have four pngs in my project, which are named frame1.png ... and so on
+        print("stripes\(counter).png")
+        
+        if counter == 25 {
+            
+            counter = 1
+            
+        }else {
+            
+            counter += 2
+        }
+        
+        testImage = UIImage(named: "stripes\(counter)-min.png")!
+        
+        //camera.stopCapture()
+        camera.removeAllTargets()
+        //blendFilter.removeAllTargets()
+        //satFilter.removeAllTargets()
+        //multiplyFilter.removeAllTargets()
+        pictureInput.removeAllTargets()
+        
+        pictureInput = PictureInput(image:testImage)
+        
+        camera --> satFilter --> blendFilter --> renderView
+        camera --> multiplyFilter
+        pictureInput --> multiplyFilter --> blendFilter
+        
+        pictureInput.processImage()
+        //camera.startCapture()
+    }
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
     }
 
-    @IBAction func stripeModeSwitch(_ sender: UISwitch) {
-        if sender.isOn {
+    @IBAction func stripeModeSwitch(_ sender: UISegmentedControl) {
+        
+        switch sender.selectedSegmentIndex
+        {
+        case 0:
             blendFilter.colorToReplace = Color.init(red: 0, green: 1, blue: 0, alpha: 1.0)
-        } else {
+        case 1:
             blendFilter.colorToReplace = Color.init(red: 1, green: 0, blue: 0, alpha: 1.0)
+        default:
+            break; 
         }
     }
     @IBAction func satChangeSlider(_ sender: UISlider) {
